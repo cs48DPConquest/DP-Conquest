0f9d50dd09605e497029361b6bae1e4410f4e4e1
Marshall Naito <Marshall Naito>
2016-01-31 19:26:39 -0800

    Implemented Trigger and new Scene for MiniGame

---
+++ DP Conquest/Assets/Scripts/YellowHouseScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class YellowHouseScript : MonoBehaviour {
+    /*
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    */
+
+    // note game levels not yet implemented
+
+    public string gameType;
+
+    void OnTriggerEnter2D(Collider2D player)
+    { 
+        SceneManager.LoadScene(gameType);
+    } 
+
+
+}
