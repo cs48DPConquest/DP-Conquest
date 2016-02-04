a757c5d5b100577296982007e4aab1ca4d33eb0c
Marshall Naito <Marshall Naito>
2016-02-04 05:17:01 -0800

    added clickable button to go back to map

---
+++ DP Conquest/Assets/Scripts/MainMapScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMapScript : MonoBehaviour {
+
+
+    public void onMapButtonPress()
+    {
+
+        SceneManager.LoadScene("MainGameScene");
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        //onMapButtonPress();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+}
