6e0c6f39bfaae41044c0ca417adc4b423784f578
Arad Reed <aradreed@gmail.com>
2016-02-11 20:14:19 -0800

    Changed camera scrolling

---
+++ DP Conquest/Assets/Scripts/CameraController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform player;
+
+	private void FixedUpdate ()
+	{
+		transform.position = Vector3.Lerp(transform.position,new Vector3(player.transform.position.x, player.transform.position.y, -10), 1);
+	}
+}
